fa10a915b57daf5713e9bff9f9b037330953f0a0
hedgehog1029 <omgredstone@gmail.com>
2015-02-27 21:15:39 +0000

    Adding a !mail or !tell function

--- app.js
+++ app.js
@@ -8,8 +8,20 @@ var config = require("./config/config.js");
 //tests
 logger.log(config.name);
 
+var mailPending = {};
+
 irc.event.on("msg", function(from, to, msg) {
     if (msg.toLowerCase().indexOf("penguin") != -1) {
         irc.client.say(to, "ALL HAIL THE OVERLORDS");
+    } else if (msg.indexOf("!") == 1) {
+        if (msg.indexOf("tell") != -1) {
+
+        }
+    }
+});
+
+irc.event.on("join", function(channel, nick) {
+    if (mailPending.indexOf(nick) != -1) {
+
     }
 });
