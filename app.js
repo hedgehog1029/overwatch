359a491623057b0dc5fabe2887409a1b76118dd3
hedgehog1029 <omgredstone@gmail.com>
2015-02-27 21:52:33 +0000

    if this shit works I will be VERY SURPRISED

--- app.js
+++ app.js
@@ -8,20 +8,31 @@ var config = require("./config/config.js");
 //tests
 logger.log(config.name);
 
-var mailPending = {};
+var visitors = [];
+var mail = [];
 
 irc.event.on("msg", function(from, to, msg) {
     if (msg.toLowerCase().indexOf("penguin") != -1) {
         irc.client.say(to, "ALL HAIL THE OVERLORDS");
-    } else if (msg.indexOf("!") == 1) {
-        if (msg.indexOf("mail") != -1) {
-
+    } else if (msg.indexOf("!") == 0) {
+        if (msg.indexOf("mail") == 1) {
+            var user = msg.substring(5, msg.indexOf(" ", 5));
+            var message = msg.substring(msg.indexOf(" ", 5), msg.length);
+            if(visitors.indexOf(user) != -1) {
+                mail.push({"nick": user, "sender": from, "msg": message});
+            }
         }
     }
 });
 
 irc.event.on("join", function(channel, nick) {
-    if (mailPending.indexOf(nick) != -1) {
-
+    if (visitors.indexOf(nick) == -1) {
+        visitors.push(nick);
     }
+    mail.every(function(value, index) {
+        if (value["nick"] == nick) {
+            irc.client.say(nick, "Mail from " + value["sender"] + ": " + value["msg"]);
+            mail.splice(index, 1);
+        }
+    });
 });
