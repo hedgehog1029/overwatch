447d8480f6ec4c265b6b2cbb6b0eaaacd84b261c
hedgehog1029 <omgredstone@gmail.com>
2015-02-27 20:46:52 +0000

    Test bot functions!

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ var config = require("./config/config.js");
 
 //tests
 logger.log(config.name);
+
+irc.on("msg", function(from, to, msg) {
+    if (s.indexOf("penguin") != -1) {
+        client.say(from, "ALL HAIL THE OVERLORDS");
+    }
+});
