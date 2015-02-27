1a1b178b328d1037c9cc9a7ad4a89e96a8632309
hedgehog1029 <omgredstone@gmail.com>
2015-02-27 22:06:00 +0000

    Changed the way !mail works, also is now !tell.

--- app.js
+++ app.js
@@ -8,34 +8,26 @@ var config = require("./config/config.js");
 //tests
 logger.log(config.name);
 
-var visitors = [];
+var recps = [];
 var mail = [];
 
 irc.event.on("msg", function(from, to, msg) {
-
-    logger.log("debug: " + msg.indexOf("!") + " and " + msg.indexOf("mail"));
+    if (from == user) {
+        mail.some(function(value) {
+            if (value["nick"] == user) {
+                irc.client.say(user, "Message from " + value["sender"] + ": " + value["msg"];
+            }
+    }
 
     if (msg.toLowerCase().indexOf("penguin") != -1) {
         irc.client.say(to, "ALL HAIL THE OVERLORDS");
     } else if (msg.indexOf("!") == 0) {
-        if (msg.indexOf("mail") == 1) {
+        if (msg.indexOf("tell") == 1) {
             var user = msg.substring(5, msg.indexOf(" ", 5));
             var message = msg.substring(msg.indexOf(" ", 5), msg.length);
-            if(visitors.indexOf(user) != -1) {
-                mail.push({"nick": user, "sender": from, "msg": message});
-            }
+            recps.push(user);
+            mail.push({"nick": user, "sender": from, "msg": message});
+            irc.client.say(from, "I'll pass that on when " + user + " is around.");
         }
     }
-});
-
-irc.event.on("join", function(channel, nick) {
-    if (visitors.indexOf(nick) == -1) {
-        visitors.push(nick);
-    }
-    mail.every(function(value, index) {
-        if (value["nick"] == nick) {
-            irc.client.say(nick, "Mail from " + value["sender"] + ": " + value["msg"]);
-            mail.splice(index, 1);
-        }
-    });
 });
