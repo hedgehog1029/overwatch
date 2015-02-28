110937350d8e2d672d326c45e478e7c634f5d78e
hedgehog1029 <omgredstone@gmail.com>
2015-02-28 09:15:40 +0000

    Complex command system part 2: mentions and any message.

--- app.js
+++ app.js
@@ -12,24 +12,5 @@ var recps = [];
 var mail = [];
 
 irc.event.on("msg", function(from, to, msg) {
-    if (recps.indexOf(from) != -1) {
-        logger.log("user is in pending mail array");
-        mail.some(function(value, index) {
-            if (recps.indexOf(value["nick"]) != -1) {
-                irc.client.say(value["nick"], "Message from " + value["sender"] + ": " + value["msg"]);
-                recps.splice(recps.indexOf(value["nick"]), 1);
-                mail.splice(index, 1);
-            }
-        });
-    }
 
-    if (msg.indexOf("!") == 0) {
-        if (msg.indexOf("tell") == 1) {
-            var user = msg.substring(6, msg.indexOf(" ", 6));
-            var message = msg.substring(msg.indexOf(" ", 6), msg.length);
-            recps.push(user);
-            mail.push({"nick": user, "sender": from, "msg": message});
-            irc.client.say(to, "I'll pass that on when " + user + " is around.");
-        }
-    }
 });
--- config/commands.js
+++ config/commands.js
@@ -1,6 +1,40 @@
+var c = require("../lib/complex-command.js");
+
 // ***** COMPLEX COMMANDS ***** //
 // Here, you may write complex  //
 // commands by using a callback //
 // to execute code when your    //
 // command is executed.         //
 
+//       ** VARIABLES **        //
+// You may register variables   //
+// for your own use here.       //
+
+var recps = [];
+var mail = [];
+
+//       ** FUNCTIONS **        //
+// This is where you register   //
+// your commands and callbacks. //
+
+c.register("tell", function(client, msg) {
+    var user = msg.substring(6, msg.indexOf(" ", 6));
+    var message = msg.substring(msg.indexOf(" ", 6), msg.length);
+
+    recps.push(user);
+    mail.push({"nick": user, "sender": from, "msg": message});
+    client.say(to, "I'll pass that on when " + user + " is around.");
+});
+
+c.registerAny(function(client, msg) {
+    if (recps.indexOf(from) != -1) {
+        logger.log("user is in pending mail array");
+        mail.some(function(value, index) {
+            if (recps.indexOf(value["nick"]) != -1) {
+                client.say(value["nick"], "Message from " + value["sender"] + ": " + value["msg"]);
+                recps.splice(recps.indexOf(value["nick"]), 1);
+                mail.splice(index, 1);
+            }
+        });
+    }
+});
