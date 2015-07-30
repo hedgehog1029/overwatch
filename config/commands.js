92f947b8b86f43c38977a141ceb40679b8629685
hedgehog1029 <omgredstone@gmail.com>
2015-07-30 10:11:09 +0100

    Updated mail system!

--- config/commands.js
+++ config/commands.js
@@ -25,20 +25,25 @@ var logger = require("../lib/log.js");
 // your commands and callbacks. //
 
 c.register("tell", function(client, from, to, msg) {
-    var user = msg.substring(6, msg.indexOf(" ", 6));
+    var user = client.client.getUserByName(msg.substring(6, msg.indexOf(" ", 6)));
     var message = msg.substring(msg.indexOf(" ", 6), msg.length);
 
     recps.push(user);
-    mail.push({"nick": user, "sender": from, "msg": message});
-    client.say(to, "I'll pass that on when " + user + " is around.");
+    mail.push({"user": user, "sender": client.client.getUserByID(from), "msg": message});
+    client.say(to, "I'll pass that on when " + user.profile.first_name + " is around.");
 });
 
 c.registerAny(function(client, from, to, msg) {
-    if (recps.indexOf(from) != -1) {
+    if (recps.indexOf(client.client.getUserByID(from)) != -1) {
         mail.some(function(value, index) {
-            if (recps.indexOf(value["nick"]) != -1) {
-                client.say(value["nick"], "Message from " + value["sender"] + ": " + value["msg"]);
-                recps.splice(recps.indexOf(value["nick"]), 1);
+            if (recps.indexOf(value["user"]) != -1) {
+                var dm;
+                client.client.openDM(value["user"].id, function(data) {
+                    dm = data.id;
+                });
+
+                client.say(dm, "Message from " + value["sender"].profile.first_name + ": " + value["msg"]);
+                recps.splice(recps.indexOf(value["user"]), 1);
                 mail.splice(index, 1);
             }
         });
@@ -60,7 +65,7 @@ c.register("g", function(client, from, to, msg) {
 		res.on("end", function() {
 			var j = JSON.parse(data);
 			if (j["items"] != null) {
-				client.say(to, from + ": " + j["items"][0]["snippet"]);
+				client.say(to, from.profile.first_name + ": " + j["items"][0]["snippet"]);
 				client.say(to, "From " + j["items"][0]["link"]);
 			}
 		});
@@ -71,15 +76,15 @@ c.register("g", function(client, from, to, msg) {
 c.register("compute", function(client, from, to, msg) {
     var searchText = msg.substring(9, msg.length);
 
-    client.say(to, from + ": computing...");
+    client.say(to, from.profile.first_name + ": computing...");
     wolfram.query(searchText, function(err, result) {
         if(err) throw err;
         if(result[1] == null) {
-            client.say(to, from + ": error computing, wolfram|alpha sucks sorry");
+            client.say(to, from.profile.first_name + ": error computing, wolfram|alpha sucks sorry");
             return;
         }
         logger.log("result: %j" + result);
-        client.say(to, from + ": " + result[1]["subpods"][0]["text"]);
+        client.say(to, from.profile.first_name + ": " + result[1]["subpods"][0]["text"]);
     });
 });
 
